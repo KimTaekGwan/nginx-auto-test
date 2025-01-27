72008472725b39c7e2c103ab7da82b167a1230da
TaekGwan Kim <sbe07032@naver.com>
2025-01-27 15:20:23 +0900

    ee

--- app/main.py
+++ app/main.py
@@ -1,27 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
 from decouple import config
-from data import ContentsServiceAPI
 
 app = FastAPI()
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
 
 
 @app.get("/")
 def read_root():
     app_env = config("APP_ENV", default="development")
     return {"message": f"Current environment: {app_env}"}
-
-
-@app.get("/announcement")
-def get_announcement():
-    api = ContentsServiceAPI()
-    return api.getAnnouncementList()
-
-
-@app.get("/notice")
-def get_notice():
-    api = ContentsServiceAPI()
-    return api.getNoticeList()
 
 
 if __name__ == "__main__":
